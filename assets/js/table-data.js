64fda8139c1c94babf4b0374d061c04065a7faa7
mane06 <tecnotop36@gmail.com>
2020-07-04 13:17:10 -0500

    version 5

--- assets/js/table-data.js
+++ assets/js/table-data.js
@@ -11,4 +11,17 @@ $(function(e) {
     { "width": "8%", "targets": 2 }
   ]
 	});
+
+	$('#usuarios').DataTable({
+		"searching": false,
+		"paging":   false,
+	    "bInfo": false,
+	    "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "ordering": false,
+        "sScrollY": "300px",
+        "columnDefs": [
+    { "width": "8%", "targets": 1 }
+  ]
+	});
 });
