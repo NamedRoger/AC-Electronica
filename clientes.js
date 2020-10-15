117967b8b5ede5a275c558b1b5d29c4405b21bb0
NamedRoger <roge.villa.carrera@hotmail.com>
2020-10-15 17:33:11 -0500

    add new filter

---
+++ clientes.js
@@ -0,0 +1,363 @@
+﻿"use strict";
+const apiUrl = "http://l.iqtics.mx/api";
+// const apiUrl = () => {
+//   const [url,protocl,hostname,path] = /^(\w+):\/\/([^\/]+)([^]+)$/.exec(window.location.href);
+//   return `${protocl}://${hostname}/api`;
+// };
+
+const formCliente = "ClienteForm";
+const currentClietnForm = "CurrentClientForm";
+const inputSearch = "input-search";
+const idTabla = "#tabla-clientes";
+
+let table;
+let modal;
+let clientes = [];
+let categorias = [];
+let precios = [];
+
+let onClickedRow = false;
+let currentSelectClient;
+
+document.addEventListener("DOMContentLoaded", Ready);
+
+async function Ready() {
+  clientes = await getClientes() || [];
+  categorias = await getCategorias() || [];
+  precios = await getPrecios() || [];
+
+  categorias.forEach(c => document.querySelector("#opCategorias").appendChild(createOption(c.id,c.nombre)));
+
+  precios.forEach(p => document.querySelector("#opPrecios").appendChild(createOption(p.id,p.nombre)));
+
+
+  let configTable = {
+    searching: false,
+    paging: false,
+    bInfo: false,
+    bScrollInfinite: true,
+    bScrollCollapse: true,
+    ordering: false,
+    sScrollY: "300px",
+    data: transformarInformacion(clientes),
+    columns: [
+      {
+        data: "representante",
+      },
+      {
+        data: "razonSocial",
+      },
+      {
+        data: "acciones",
+      },
+    ],
+    createdRow: function (row, data, dataIndex) {
+      $(row).attr("id", data.id);
+      row.addEventListener("click", selectRow);
+    },
+  };
+
+  table = createTable(idTabla, configTable);
+  loadButtons();
+  
+  modal = $("#FormModal");
+  // modal.on("show.bs.modal", () => console.log("ssss"));
+  modal.on("hidden.bs.modal", () => resetForm(formCliente));
+
+
+  document.getElementById(formCliente).addEventListener("submit",onSubmit);
+  document.getElementById(inputSearch).addEventListener("keyup",(e) => {
+    const text = e.target.value;
+    table.search(text).draw();
+    // const clientesFiltrados = clientes.filter(c => filtrarClientes(c,text));
+
+    // resetForm(currentClietnForm);
+    // reloadTable(table,transformarInformacion(clientesFiltrados));
+  });
+  document.getElementById("btn-export").addEventListener("click",exportXls);
+}
+
+async function getClientes() {
+  const result = await HttpClient.get(`${apiUrl}/clientes`);
+  return result || [];
+}
+
+async function addCliente(newCliente){
+  const result = await HttpClient.post(`${apiUrl}/clientes`,newCliente);
+  if(result !== undefined && result !== null ){
+    window.location.reload();
+  }
+}
+
+async function editClient(id,cliente){
+  const result = await HttpClient.put(`${apiUrl}/clientes/${id}`,cliente);
+  window.location.reload();
+}
+
+async function deleteCliente(idCliente){
+  const result = await HttpClient.delete(`${apiUrl}/clientes/${idCliente}`);
+  clientes.splice(findClientIndex(idCliente),1);
+  reloadTable(table,transformarInformacion(clientes));
+}
+
+async function exportXls(e){
+  const result = await HttpClient.get(`${apiUrl}/clientes/export`);
+  if(result === undefined){
+    alert("Error al exportar");
+    return;
+  }
+  const url = window.URL.createObjectURL(result);
+
+  const a = document.createElement("a");
+  a.setAttribute("download","Clientes.xlsx");
+  a.href = url;
+  a.click();
+  a.remove();
+
+  window.URL.revokeObjectURL(url);
+}
+
+async function getCategorias() {
+  const result = await HttpClient.get(`${apiUrl}/opcionMenus/CategoriaMenu/categoriacliente`);
+  return result;
+}
+
+async function getPrecios(){
+  const result = await HttpClient.get(`${apiUrl}/opcionMenus/CategoriaMenu/preciocliente`);
+  return result;
+}
+
+const findClientIndex = (idCliente) =>
+  clientes.findIndex((cliente) => cliente.id === idCliente);
+
+function filtrarClientes(cliente, str) {
+  const clienteJoin = Object.values(cliente).join("");
+  return clienteJoin.includes(str);
+}
+
+const createTable = (table, config) => $(table).DataTable(config);
+
+function reloadTable(table, data) {
+  table.clear().draw();
+  table.rows.add(data).draw();
+  loadButtons();
+}
+
+function loadButtons() {
+  const btnEditar = document.querySelectorAll(".btn-editar");
+  btnEditar.forEach((btn) => btn.addEventListener("click", onClickEdit));
+  
+  const btnBorrar = document.querySelectorAll(".btn-eliminar");
+  btnBorrar.forEach((btn) => btn.addEventListener("click",onClickDelete));
+}
+
+function onClickEdit(e) {
+  const cliente = clientes[findClientIndex(e.target.id)];
+  llenarForm(formCliente, cliente);
+  $(modal).modal({
+    show: true,
+    backdrop: "static",
+  });
+  e.stopImmediatePropagation();
+}
+
+function onClickDelete(e){
+  const cliente = clientes[findClientIndex(e.target.id)];
+  const desicion = confirm("¿Deseas borrar el cliente?");
+  if(desicion){
+    deleteCliente(cliente.id);
+  }
+  e.stopImmediatePropagation();
+}
+
+function onKeyUp(e){
+  const value = e.target.value;
+}
+
+function onSubmit(e){
+  e.preventDefault();
+  const form = e.target;
+  const id = form.elements["Id"].value;
+  
+  if(id === undefined || id === ""){
+    addCliente(new FormData(form));
+  }else{
+    editClient(id,new FormData(form));
+  }
+}
+
+function selectRow(e) {
+  const rows = [
+    ...e.path.find((r) => r.nodeName === "TBODY".toUpperCase()).children,
+  ];
+  rows.forEach((row) => removeClass(row, "row-selected"));
+
+  const row = e.path.find((r) => r.nodeName === "tr".toUpperCase());
+  const cliente = clientes[findClientIndex(row.id)];
+
+  if (currentSelectClient !== undefined && currentSelectClient !== cliente) {
+    // row.classList.add("row-selected");
+    addClass(row, "row-selected");
+    llenarForm(currentClietnForm, cliente);
+    currentSelectClient = cliente;
+    onClickedRow = true;
+  } else {
+    if (onClickedRow) {
+      resetForm(currentClietnForm);
+      onClickedRow = false;
+    } else {
+      addClass(row, "row-selected");
+      llenarForm(currentClietnForm, cliente);
+      currentSelectClient = cliente;
+      onClickedRow = true;
+    }
+  }
+}
+
+const createOption = (value,text) => {
+  const op = document.createElement("option");
+  op.value = value;
+  op.innerText = text;
+  return op;
+}
+
+function removeClass(node, className) {
+  if (node.classList.contains(className)) node.classList.remove(className);
+}
+
+function addClass(node, className) {
+  if (!node.classList.contains(className)) node.classList.add(className);
+}
+
+const transformarInformacion = (data) =>
+  data.map(
+    (entity) =>
+      new InformacionTabla(entity.id, entity.representante, entity.razonSocial)
+  );
+
+// llenar formularios
+function llenarForm(idForm, entity) {
+  const inputs = {
+    value: ["text", "number", "select-one", "date"],
+    check: ["checkbox"],
+  };
+  const fomInputs = [...document.getElementById(idForm).elements];
+  const keys = Object.keys(entity);
+
+  fomInputs.forEach((input) => {
+    if (inputs.value.includes(input.type)) {
+      const key = keys.find(
+        (k) => k.toUpperCase() === input.name.toUpperCase()
+      );
+      input.value = entity[key] || "";
+    }
+    if (inputs.check.includes(input.type)) {
+      const key = keys.find(
+        (k) => k.toUpperCase() === input.name.toUpperCase()
+      );
+      input.checked = entity[key] || false;
+    }
+  });
+}
+
+function resetForm(form) {
+  document.getElementById(form).reset();
+}
+
+class HttpClient {
+  static async get(url) {
+    const result = await fetch(url)
+      .then(async (res) => {
+        if(res.ok){ 
+          const contentType = res.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            return await res.json();
+          }
+          else if (contentType && 
+            contentType.indexOf("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") !== -1){
+              return await res.blob();
+          }
+          else {
+            console.log("Oops, we haven't got JSON!");
+          }
+        }else {
+           throw "Error";
+        }
+      })
+      .catch((error) => console.log(error));
+
+    return result;
+  }
+
+  static post(url, data) {
+    const result = fetch(url, {
+      method: "post",
+      body: data,
+    }).then(async res => {
+      if(res.ok){
+        const contentType = res.headers.get("content-type");
+        if (contentType && contentType.indexOf("application/json") !== -1) {
+          return await res.json();
+        } else {
+          console.log("Oops, we haven't got JSON!");
+        }
+      }else {
+        throw "Error";
+      }
+      
+    })
+    .catch(err => console.log(err));
+
+    return result;
+  }
+
+  static put(url, data) {
+    return fetch(url, {
+      method: "put",
+      body: data,
+    }).then(async res => {
+      if(res.ok){
+      const contentType = res.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        return await res.json();
+      } else {
+        console.log("Oops, we haven't got JSON!");
+      }
+      }else{
+        throw "Error";
+      }
+    })
+    .catch(err => console.log(err));;
+  }
+
+  static delete(url) {
+    return fetch(url, {
+      method: "delete",
+    });
+  }
+}
+
+class InformacionTabla {
+  constructor(id, representante, razonSocial) {
+    this.id = id;
+    this.representante = representante;
+    this.razonSocial = razonSocial;
+    this.acciones = `
+      <button class="btn btn-sm btn-primary btn-editar" id="${id}">
+        Editar
+      </button>
+      <button class="btn btn-sm btn-danger btn-eliminar" id="${id}">
+        Eliminar
+      </button>
+    `;
+  }
+}
+
+class OptionMenu {
+  constructor(value,text){
+    this.value = value;
+    this.text = text;
+  }
+}
+
+
---
+++ proveedores.js
@@ -0,0 +1,346 @@
+﻿
+
+const apiUrl = "http://l.iqtics.mx/api";
+
+
+class ProveedorTabla {
+  constructor(id, represenante, razonSocial) {
+    this.id = id;
+    this.representante = represenante;
+    this.razonSocial = razonSocial;
+    this.acciones = `
+            <button class="btn btn-info btn-sm btn-editar" type="button" data-toggle="modal" data-client-id="${id}" data-backdrop="static" data-keyboard="false" data-target="#EditarCliente">
+                <i class="fas fa-user-edit"></i>
+            </button>
+            <button class="btn btn-danger btn-sm btn-eliminar" type="submit"  data-client-id="${id}">
+                <i class="fas fa-user-times"></i>
+            </button>
+        `;
+  }
+}
+
+class ProveedorFiltro {
+  constructor(
+    id,
+    claveRegistro,
+    representante,
+    telefono,
+    celular,
+    email,
+    ciudad,
+    razonSocial,
+    rfc,
+    telefonoEmpresa,
+    celularEmpresa,
+    ciudadEmpresa,
+    paqueteria
+  ) {
+    this.id = id;
+    this.claveRegistro = claveRegistro;
+    this.representante = representante;
+    this.telfono = telefono;
+    this.celular = celular;
+    this.email = email;
+    this.ciudad = ciudad;
+    this.razonSocial = razonSocial;
+    this.rfc = rfc;
+    this.telefonoEmpresa = telefonoEmpresa;
+    this.celularEmpresa = celularEmpresa;
+    this.ciudadEmpresa = ciudadEmpresa;
+    this.paqueteria = paqueteria;
+  }
+}
+
+let proveedores = [];
+let proveedoresFilter = [];
+let proveedorSeleccionado = {};
+
+(() => {
+  document.addEventListener("DOMContentLoaded", async () => {
+    let formFiltros = document.querySelector("#form-filtros");
+    let formMostrarCliente = document.querySelector("#mostrar-proveedor");
+    let inputsFechaAlta = document.querySelectorAll(".input-fecha-alta");
+    proveedores = await GetProveedores();
+    let tabla = CrearTabla(TransoformarInformacion(proveedores));
+    formFiltros.addEventListener("keyup", () => {
+      let filtro = formFiltros.value;
+      proveedoresFilter = proveedores.filter((c) =>
+        FiltrarProveedores(c, filtro)
+      );
+      RecargarTabla(tabla, TransoformarInformacion(proveedoresFilter));
+    });
+    document
+      .getElementById("add-proveedor")
+      .addEventListener("submit", onSubmitAddForm);
+    document
+      .getElementById("edit-proveedor")
+      .addEventListener("submit", onSubmitEditForm);
+    document.getElementById("btn-export").addEventListener("click", exportXls);
+  });
+})();
+
+function onSubmitAddForm(e) {
+  const form = e.target;
+  PostProveedor(new FormData(form));
+  reload();
+}
+
+function onSubmitEditForm(e) {
+  const form = e.target;
+  PutProveedor(new FormData(form));
+  reload();
+}
+
+async function exportXls(e) {
+  const result = await HttpClient.get(`${apiUrl}/proveedores/export`);
+  console.log(result);
+  const url = window.URL.createObjectURL(result);
+
+  const a = document.createElement("a");
+  a.setAttribute("download", "Clientes.xlsx");
+  a.href = url;
+  a.click();
+  a.remove();
+
+  window.URL.revokeObjectURL(url);
+}
+
+async function PostProveedor(proveedor) {
+  const result = await HttpClient.post(`${apiUrl}/proveedores`, proveedor);
+  console.log(result);
+}
+
+async function PutProveedor(proveedor) {
+  const result = await HttpClient.put(
+    `${apiUrl}/proveedores/` + proveedorSeleccionado.id,
+    proveedor
+  );
+  console.log(result);
+}
+
+async function GetProveedores() {
+  const result = await HttpClient.get(`${apiUrl}/proveedores`);
+  return result;
+}
+
+function FindProveedoresOfIdx(id) {
+  let idx = proveedores.findIndex((proveedor) => proveedor.id === id);
+  return idx;
+}
+
+async function EliminarProveedor(IdCliente) {
+  let data = IdCliente;
+  const result = await HttpClient.delete(`${apiUrl}/proveedores/` + data);
+  let clienteDeleted = await result.json();
+  proveedores.splice(FindProveedoresOfIdx(clienteDeleted.id), 1);
+}
+
+function SeleccionarProveedor(idProveedor) {
+  let cliente = proveedores[FindProveedoresOfIdx(idProveedor)];
+  llenarForm("#mostrar-proveedor", cliente);
+}
+
+function CrearTabla(data) {
+  let tabla = $("#tabla").DataTable({
+    searching: false,
+    paging: false,
+    bInfo: false,
+    bScrollInfinite: true,
+    bScrollCollapse: true,
+    ordering: false,
+    sScrollY: "170px",
+    columnDefs: [{ width: "10%", targets: 2 }],
+    createdRow: function (row, data, dataIndex) {
+      $(row).attr("id", data.id);
+    },
+    columns: [
+      { data: "representante" },
+      { data: "razonSocial" },
+      { data: "acciones" },
+    ],
+    data: data,
+  });
+
+  SelectFila(tabla);
+  CargarBotones(tabla);
+  return tabla;
+}
+
+function SelectFila(tabla) {
+  let filas = [...document.querySelectorAll("#tabla tbody tr")];
+
+  filas.forEach((tr) => {
+    tr.addEventListener("click", () => {
+      if (tr.classList.contains("selected")) {
+        tr.classList.remove("selected");
+        tr.style.backgroundColor = "";
+        tr.style.color = "";
+        document.getElementById("mostrar-proveedor").reset();
+      } else {
+        tabla.$("tr.selected").removeClass("selected");
+        tr.classList.add("selected");
+        for (var i = 0; i < filas.length; i++) {
+          filas[i].style.backgroundColor = "";
+          filas[i].style.color = "";
+        }
+        tr.style.backgroundColor = "#1560ff";
+        tr.style.color = "#fff";
+        SeleccionarProveedor(tr.id);
+      }
+    });
+  });
+}
+
+function TransoformarInformacion(data) {
+  let dataResult = [];
+  data.forEach((proveedor) =>
+    dataResult.push(
+      new ProveedorTabla(
+        proveedor.id,
+        proveedor.representante,
+        proveedor.razonSocial
+      )
+    )
+  );
+  return dataResult;
+}
+
+function FiltrarProveedores(proveedor, str) {
+  let datosProveedor = Object.values(proveedor).join("");
+  return datosProveedor.includes(str);
+}
+
+function reload() {
+  window.location.reload();
+}
+
+async function CargarBotones(tabla) {
+  let btnElimniar = document.querySelectorAll(".btn-eliminar");
+  let btnEditar = document.querySelectorAll(".btn-editar");
+
+  btnElimniar.forEach((btn) => {
+    let idCliente = btn.dataset.clientId;
+    btn.addEventListener("click", () => {
+      const confirmacion = confirm("¿Desea borrar el proveedor?");
+      if (confirmacion) {
+        EliminarProveedor(idCliente);
+        reload();
+      }
+    });
+  });
+  btnEditar.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let id = FindProveedoresOfIdx(btn.dataset.clientId);
+      console.log(id);
+      proveedorSeleccionado = proveedores[id];
+      console.log(proveedorSeleccionado);
+      console.log(proveedorSeleccionado.id);
+      llenarForm("#edit-proveedor", proveedorSeleccionado);
+    });
+  });
+}
+
+function llenarForm(idForm, entity) {
+  const inputs = {
+    value: ["text", "number", "select", "date"],
+    check: ["checkbox"],
+  };
+
+  const fomInputs = [...document.querySelector(idForm).elements];
+  
+  if(entity !== undefined){
+    const keys = Object.keys(entity);
+
+    fomInputs.forEach((input) => {
+      if (inputs.value.includes(input.type)) {
+        const key = keys.find(
+          (k) => k.toUpperCase() === input.name.toUpperCase()
+        );
+        input.value = entity[key];
+      }
+      if (inputs.check.includes(input.type)) {
+        const key = keys.find(
+          (k) => k.toUpperCase() === input.name.toUpperCase()
+        );
+        input.checked = entity[key];
+      }
+    });
+  }
+  
+}
+
+class HttpClient {
+  static async get(url) {
+    const result = await fetch(url)
+      .then(async (res) => {
+        if (res.ok) {
+          const contentType = res.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            return await res.json();
+          } else if (
+            contentType &&
+            contentType.indexOf(
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            ) !== -1
+          ) {
+            return await res.blob();
+          } else {
+            console.log("Oops, we haven't got JSON!");
+          }
+        } else {
+          throw "Error";
+        }
+      })
+      .catch((error) => console.log(error));
+
+    return result;
+  }
+
+  static post(url, data) {
+    const result = fetch(url, {
+      method: "post",
+      body: data,
+    })
+      .then(async (res) => {
+        if (res.ok) {
+          const contentType = res.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            return await res.json();
+          } else {
+            console.log("Oops, we haven't got JSON!");
+          }
+        } else {
+          throw "Error";
+        }
+      })
+      .catch((err) => console.log(err));
+
+    return result;
+  }
+
+  static put(url, data) {
+    return fetch(url, {
+      method: "put",
+      body: data,
+    })
+      .then(async (res) => {
+        if (res.ok) {
+          const contentType = res.headers.get("content-type");
+          if (contentType && contentType.indexOf("application/json") !== -1) {
+            return await res.json();
+          } else {
+            console.log("Oops, we haven't got JSON!");
+          }
+        } else {
+          throw "Error";
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static delete(url) {
+    return fetch(url, {
+      method: "delete",
+    });
+  }
+}
